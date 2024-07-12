1b12b3b46180add9999cb645c64a54cee5daa50c
Brian Tran <minhtritran.dev@gmail.com>
2024-07-12 17:08:10 +1000

    Spring Initializr for warptracker

---
+++ backend/warptracker/src/main/java/com/example/warptracker/WarptrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.warptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarptrackerApplication.class, args);
+	}
+
+}
---
+++ backend/warptracker/src/test/java/com/example/warptracker/WarptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
