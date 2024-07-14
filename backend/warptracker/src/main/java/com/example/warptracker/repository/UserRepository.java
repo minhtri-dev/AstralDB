868211fcb207f28bc9999f421c3454c92537b58e
Brian Tran <minhtritran.dev@gmail.com>
2024-07-14 17:27:52 +1000

    updated repo

--- backend/warptracker/src/main/java/com/example/warptracker/repository/UserRepository.java
+++ backend/warptracker/src/main/java/com/example/warptracker/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.example.warptracker.repository;
 
-import com.example.warptracker.models.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.warptracker.model.user;
 
 @Repository
 public interface UserRepository extends JpaRepository<user, Integer> {
--- backend/warptracker/src/main/java/com/example/warptracker/repository/WarpRepository.java
+++ backend/warptracker/src/main/java/com/example/warptracker/repository/WarpRepository.java
@@ -1,8 +1,9 @@
 package com.example.warptracker.repository;
 
-import com.example.warptracker.models.warp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.warptracker.model.warp;
 
 @Repository
 public interface WarpRepository extends JpaRepository<warp, Integer> {
