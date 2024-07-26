79f1d24a5c7a375d1fc3499858e501744a2fada9
Brian Tran <minhtritran.dev@gmail.com>
2024-07-26 10:40:51 +1000

    tailwind

---
+++ frontend/astraldb/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'login': '#00ADC9',
+        'login-hover': '#0088A3',
+      },
+    },
+  },
+  plugins: [],
+}
+
