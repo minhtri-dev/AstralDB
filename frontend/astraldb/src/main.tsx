b610f5b9defe85687632184dbc5ca1d6bbd9ef37
Brian Tran <minhtritran.dev@gmail.com>
2025-04-29 19:30:57 +1000

    Tailwind and removed redundant code

--- frontend/astraldb/src/main.tsx
+++ frontend/astraldb/src/main.tsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+function App() {
+  return (
+    <>
+      <p className='text-amber-300 font-bold underline'>
+        tailwind tests
+      </p>
+    </>
+  )
+}
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- frontend/astraldb/vite.config.ts
+++ frontend/astraldb/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default {
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
 } satisfies UserConfig
