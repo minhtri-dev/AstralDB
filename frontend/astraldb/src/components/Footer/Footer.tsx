c851e8036d689f8595b956748b9301081f7a3a9f
Brian Tran <minhtritran.dev@gmail.com>
2025-05-01 21:36:57 +1000

    Implemented footer

--- frontend/astraldb/src/components/Footer/Footer.tsx
+++ frontend/astraldb/src/components/Footer/Footer.tsx
@@ -1,7 +1,18 @@
+import { Icon } from '@iconify-icon/react';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-[#1c2029] text-white text-center py-4 mt-auto">
+      <a
+			href="https://github.com/minhtri-dev/AstralDB"
+			className="duration-150 hover:text-white"
+			target="_blank"
+		>
+			<Icon icon={"mdi:github"} height={40} />
+		</a>
+    <p>View on GitHub</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
