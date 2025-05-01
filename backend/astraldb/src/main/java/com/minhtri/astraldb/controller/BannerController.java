05fc6369e4820e2fbe707e85d94e0d06b40e7fe2
Brian Tran <minhtritran.dev@gmail.com>
2025-05-01 21:53:47 +1000

    Added CrossOrigin

--- backend/astraldb/src/main/java/com/minhtri/astraldb/controller/BannerController.java
+++ backend/astraldb/src/main/java/com/minhtri/astraldb/controller/BannerController.java
@@ -1,6 +1,7 @@
 package com.minhtri.astraldb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class BannerController {
     @Autowired
     BannerRepository bannerRepository;
     
+    @CrossOrigin
     @GetMapping
     public List<Banner> getAllBanners() {
         return bannerRepository.findAll();
